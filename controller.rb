c919fe9ada3921bb672fa7272b688d96dc72f633
Travis Grimmond <travisslack1994@outlook.com>
2017-07-17 17:40:00 +0100

    Extension done

--- controller.rb
+++ controller.rb
@@ -3,8 +3,19 @@ require('sinatra/contrib/all')
 require_relative('./models/game')
 require('json')
 
+get '/' do
+  erb(:home)
+end
+
+get '/the-rules' do
+  erb(:the_rules)
+end
+
 get '/play/:hand1/:hand2' do
   game = Game.new( params[:hand1], params[:hand2] )
-  return "#{game.play()}" 
-end
+  # return "#{game.play()}" 
+    @game_winner = game.play
+    erb( :result )
+  end
+
 
